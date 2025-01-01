c9ca8b8c54ecf54eabe9f3e3f36aef674e09691f
Aaquib07 <aaquibasrar4@gmail.com>
2025-01-01 23:29:50 +0530

    Initial commit

---
+++ backend/main.py
@@ -0,0 +1,93 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
+from fastapi.middleware import cors
+from PIL import Image
+import torch.nn as nn
+import torch
+import uvicorn
+from torchvision import transforms
+import os
+
+IMAGE_SIZE = 150
+
+app = FastAPI()
+app.add_middleware(
+    cors.CORSMiddleware,
+    allow_origins=[os.getenv('CLIENT_URL')],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+
+torch.cuda.empty_cache()
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Define the CNN model
+class BrainTumorModel(nn.Module):
+    def __init__(self):
+        super(BrainTumorModel, self).__init__()
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+        self.fc_layers = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(256 * 9 * 9, 128),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(64, 4)
+        )
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = self.fc_layers(x)
+        return x
+    def predict(self, x):
+        with torch.no_grad():
+            x = self.forward(x)
+            return torch.softmax(x, dim=1)
+        
+
+checkpoint = torch.load('model/best_model.pt', map_location=device, weights_only=True)
+model = BrainTumorModel().to(device)
+model.load_state_dict(checkpoint['model_state_dict'])
+model.eval()
+
+transform = transforms.Compose([
+    # transforms.ToPILImage(),
+    transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5], [0.5])
+])
+
+labels = ['glioma tumor', 'meningioma tumor', 'no tumor', 'pituitary tumor']
+
+@app.post("/classify/")
+async def classify_image(file: UploadFile = File(...)):
+    try:
+        image = Image.open(file.file).convert('RGB')
+        image = transform(image).unsqueeze(0).to(device)
+
+        outputs = model(image)
+        _, predicted = torch.max(outputs, 1)
+
+        return JSONResponse(content={'class': labels[predicted.item()]})
+    except Exception as e:
+        return JSONResponse(content={'error': str(e)}, status_code=500)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=5000, reload=True)
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,256 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+  Paper,
+  Box,
+  Grid2
+} from "@mui/material";
+import Brain from "../assets/brain.png";
+import { styled } from "@mui/system";
+import { useNavigate } from "react-router-dom";
+
+
+const UploadButton = styled(Button)({
+  marginTop: "20px",
+  backgroundColor: "#2196f3",
+  color: "#fff",
+  padding: "10px 20px",
+  fontSize: "18px",
+  "&:hover": {
+    backgroundColor: "#1976d2",
+  },
+});
+
+const PredictButton = styled(Button)({
+  marginTop: "20px",
+  marginLeft: "15px",
+  backgroundColor: "#ff5722",
+  color: "#fff",
+  padding: "10px 20px",
+  fontSize: "18px",
+  "&:hover": {
+    backgroundColor: "#e64a19",
+  },
+});
+
+const Home = () => {
+  const SERVER = process.env.SERVER_URL
+  const navigate = useNavigate();
+  const [file, setFile] = useState(null);
+  const [preview, setPreview] = useState(null);
+  const [prediction, setPrediction] = useState("");
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setPreview(reader.result);
+      };
+      reader.readAsDataURL(selectedFile);
+    }
+  };
+
+  const handleUpload = async () => {
+    if (!file) {
+      alert("Please select a file first!");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const response = await fetch(`${SERVER}/classify/`, {
+        method: "POST",
+        body: formData,
+      });
+      const result = await response.json();
+      setPrediction(result.class || result.error);
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      setPrediction("An error occurred while classifying.");
+    }
+  };
+
+  return (
+    <div>
+      <AppBar position="static" style={{ backgroundColor: "#2c3e50" }}>
+        <Toolbar>
+          <Typography variant="h3" style={{ flexGrow: 1, padding: "10px" }}>
+            Brain Tumor Classification
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Container
+        style={{
+          minHeight: "80vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <Grid2
+          container
+          spacing={4}
+          marginBottom={10}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid2 item xs={12} md={6}>
+            <Typography
+              variant="h3"
+              gutterBottom
+              style={{ color: "#2c3e50", fontWeight: "bold", padding: '10px' }}
+            >
+              Detect Brain Tumors with Deep Learning
+            </Typography>
+            <Typography
+              variant="h6"
+              gutterBottom
+              style={{ color: "#34495e", lineHeight: 1.5 }}
+            >
+              Our advanced platform enables accurate and fast detection of brain
+              tumors from MRI images, providing reliable results for medical
+              professionals and patients.
+            </Typography>
+
+            {preview && (
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginTop: "20px",
+                  marginBottom: "20px",
+                }}
+              >
+                <Paper
+                  elevation={4}
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    padding: "10px",
+                    textAlign: "center",
+                    backgroundColor: "#ecf0f1",
+                  }}
+                >
+                  <img
+                    src={preview}
+                    alt="Preview"
+                    style={{
+                      width: "100%",
+                      maxWidth: "40px",
+                      height: "auto",
+                      objectFit: "cover",
+                      borderRadius: "10px",
+                      border: "1px solid #ccc",
+                    }}
+                  />
+                  <Typography
+                    variant="p"
+                    gutterBottom
+                    style={{ color: "#2c3e50" }}
+                  >
+                    {file.name}
+                  </Typography>
+                </Paper>
+              </Box>
+            )}
+
+            <Box display="flex" justifyContent="center" alignItems="center">
+              <UploadButton component="label">
+                Upload MRI Image
+                <input type="file" hidden onChange={handleFileChange} />
+              </UploadButton>
+              <PredictButton onClick={handleUpload}>Predict</PredictButton>
+            </Box>
+
+            {prediction && (
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginTop: "20px",
+                  marginBottom: "20px",
+                }}
+              >
+                <Paper
+                  elevation={4}
+                  style={{
+                    padding: "20px",
+                    backgroundColor: "#ffffff",
+                    textAlign: "center",
+                    width: "50%",
+                    border: "2px solid #4caf50",
+                  }}
+                >
+                  <Typography
+                    variant="h5"
+                    style={{ color: "#2c3e50", fontWeight: "bold" }}
+                  >
+                    Prediction Result
+                  </Typography>
+                  <Typography
+                    variant="h6"
+                    style={{ color: "#34495e", marginTop: "10px" }}
+                  >
+                    {prediction}
+                  </Typography>
+                </Paper>
+              </Box>
+            )}
+
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                marginTop: '50px',
+              }}
+            >
+              <Paper
+                elevation={4}
+                style={{
+                  width: "20%",
+                  padding: "10px",
+                  backgroundColor: "#ecf0f1",
+                  textAlign: "center",
+                }}
+              >
+                <img
+                  src={Brain}
+                  alt="MRI Upload"
+                  style={{ width: "100%", borderRadius: "10px" }}
+                />
+              </Paper>
+            </Box>
+          </Grid2>
+        </Grid2>
+      </Container>
+
+      <Box
+        sx={{
+          backgroundColor: "#2c3e50",
+          color: "#ecf0f1",
+          padding: "20px",
+          textAlign: "center",
+          marginTop: "30px",
+        }}
+      >
+        <Typography variant="subtitle1">
+          © 2025 Brain Tumor Classification | All rights reserved.
+        </Typography>
+      </Box>
+    </div>
+  );
+};
+
+export default Home;
